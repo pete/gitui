06061bef1fe698ff1d5536af0a6757602dc243f0
Pete Elmore <1337p337@gmail.com>
2009-02-16 17:14:18 -0800

    Restructure command decisions.

--- lib/git_ui.rb
+++ lib/git_ui.rb
@@ -1,6 +1,19 @@
 require 'colorize'
 
 module GitUI
+	class Action
+		attr_accessor :key, :desc, :block
+		def initialize(k, d, &b)
+			@key = k
+			@desc = d
+			@block = b
+		end
+	end
+
+	def act *a, &b
+		Action.new(*a, &b)
+	end
+
 	def compose *lambdas
 		lambda { |*args|
 			lambdas.reverse.inject(args) { |acc, f|
@@ -23,6 +36,46 @@ module GitUI
 		}
 	end
 
+	def add_untracked
+		cs = untracked_files
+		if cs.empty?
+			puts "No new files; try your .gitignore, maybe?".colorize(:cyan)
+			return []
+		end
+
+		i = 0
+
+		while i < cs.size
+			puts "#{cs[i][:filename]} (#{i + 1}/#{cs.size})"
+			print "Add? [ynvpdaqjk]: ".colorize(:cyan)
+			$stdout.flush
+			inp = $stdin.read(1)
+			print "\n"
+		end
+	end
+
+	def decide prompt, acts, color = :default
+		keys = acts.map { |act| act.key } << '?'
+		prompt = "#{prompt} [#{keys.join('')}]: ".colorize(color)
+
+		act = nil
+		loop {
+			print "#{prompt} [#{keys.join('')}]: ".colorize(color)
+			$stdout.flush
+			inp = $stdin.read(1)
+			print "\n"
+			if inp == '?'
+				puts acts.map { |a|
+					"  #{a.key}: #{a.desc}"
+				}
+			elsif a = acts.find { |a| inp == a.key }
+				return a.block.call
+			else
+				print "No such action:  #{inp}"
+			end
+		}
+	end
+
 	def record *args
 		cs = changes
 		if cs.empty?
@@ -30,37 +83,29 @@ module GitUI
 			return []
 		end
 		i = 0
+
+		acts = [
+			act('y', "Yes, commit this change.") { 
+				cs[i][:action] = :commit; i += 1 },
+			act('n', 'No, don\'t commit this change.') {
+				cs[i][:action] = nil; i += 1 },
+			act('v', 'View this patch') {
+				system "git diff #{cs[i][:filename]}" },
+			act('p', 'View this patch in a pager') {
+				system "git diff #{cs[i][:filename]} | #{pager}" },
+			act('d', 'Done, skip to commit step.') { i = cs.size },
+			act('a', 'Record all remaining') {
+				cs[i..-1].each { |c| c[:action] = :record }
+				i = cs.size },
+			act('q', 'Quit, skip commit step.') { cs.clear },
+			act('j', 'Jump to next patch in list') { i += 1 },
+			act('k', 'Go back to previous patch in list') { 
+				i -= 1; i = 0 if i < 0 },
+		]
+
 		while i < cs.size
 			puts "#{cs[i][:filename]} #{cs[i][:status]} (#{i + 1}/#{cs.size})"
-			print "Record? [ynvpdaqjk]: ".colorize(:cyan)
-			$stdout.flush
-			inp = $stdin.read(1)
-			print "\n"
-			case inp
-			when 'y'
-				cs[i][:action] = :commit
-				i += 1
-			when 'n'
-				cs[i][:action] = nil
-				i += 1
-			when 'v'
-				system "git diff #{cs[i][:filename]}"
-			when 'p'
-				system "git diff #{cs[i][:filename]} | #{pager}"
-			when 'd'
-				i = cs.size
-			when 'a'
-				cs[i..-1].each { |c| c[:action] = :record }
-				i = cs.size
-			when 'q'
-				return []
-			when 'j'
-				i += 1
-			when 'k'
-				i -= 1; i = 0 if i < 0
-			else
-				puts "Invalid option: #{inp.inspect}!"
-			end
+			decide "Record?", acts, :cyan
 		end
 
 		cs
@@ -68,6 +113,7 @@ module GitUI
 
 	def commit(*cs)
 		fs = cs.select { |c| c[:action] == :commit }.map { |c| c[:filename] }
+		return if fs.empty?
 		system "git commit #{fs.join(' ')}"
 	end
 
@@ -75,13 +121,22 @@ module GitUI
 		stty_cooked = lambda { |*args| system "stty sane"; args }
 		stty_raw = lambda { |*args| system "stty -icanon min 1"; args }
 
-		case args.first
+		cmd, *args = args
+
+		case cmd
 		when /^rec/i
 			lambda {
 				compose(stty_cooked, 
 						method(:commit),
 						method(:record), 
-						stty_raw)[*args[1..-1]]
+						stty_raw)[*args]
+			}
+		when /^add/i
+			lambda {
+				compose(stty_cooked,
+						method(:commit),
+						method(:add_untracked),
+						stty_raw)[*args]
 			}
 		else
 			nil
